cc83e5c88789892be64fd1798699bf931494e7c8
Yumi <100600260+yumi-nakadera@users.noreply.github.com>
2024-08-22 09:55:16 +0200

    Update transdecoder.sh

--- script/exp2/transdecoder.sh
+++ script/exp2/transdecoder.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-#SBATCH --job-name=transdecoder
-#SBATCH -p defq
-#SBATCH --ntasks-per-node=1
-#SBATCH --nodes=1
-#SBATCH -c 16
-#SBATCH --mem=100G
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=y.nakadera@vu.nl
-#SBATCH -o %x_%A_%a.out
-#SBATCH -e %x_%A_%a.err
 
 module load TransDecoder #TransDecoder/5.5.0
 
